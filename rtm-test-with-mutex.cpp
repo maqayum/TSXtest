0d88cedff0592f20bbad23045a8e57514fe8d908
Mohammd A Qayum <maqayum@gmail.com>
2015-01-21 00:48:39 -0700

    an update

--- rtm-test-with-mutex.cpp
+++ rtm-test-with-mutex.cpp
@@ -1,22 +1,55 @@
 #include <iostream>
-#include <mutex>
+//#include <mutex>
+#include <sys/syscall.h>
+#include <atomic>
 #include <omp.h>
 #include <immintrin.h>
 
 #define N 1000
 using namespace std;
 
+class SimpleSpinLock
+{
+	volatile unsigned int state;
+
+	enum { Free = 0, Busy = 1};
+
+public:
+	SimpleSpinLock() : state(Free) {}
+
+	void lock()
+	{
+//		while(InterlockedCompareExchange(&state,Busy,Free) != Free)
+		while(__sync_lock_test_and_set(&state,Busy,Free) != Free)
+		{
+			do { _mm_pause(); } while(state == Busy);
+		}
+	}
+
+	void unlock() { __sync_lock_release(&state, Free);  }
+
+	bool isLocked() const { return state == Busy; }
+
+};
+
 int main() {
-mutex fb_mutex; // fallback mutex
+
+//mutex fb_mutex; // fallback mutex
+SimpleSpinLock fallBackLock;
+
 int i, group[N], data[N], sums[N];
 
+for(i = 0; i < N; i++){
+    group[i]=i;
+    data[i]=i;
+}
 
-#pragma omp parallel for num_threads(8)
 
+#pragma omp parallel for num_threads(100)
 for(i = 0; i < N; i++){
    int mygroup = group[i];
    if(_xbegin()) {  // First attempt
-       if( !fb_mutex.is_acquired() ) {
+       if( !fallBackLock.isLocked() ) {
            sums[mygroup] += data[i];
        }
        else{
@@ -24,9 +57,13 @@ for(i = 0; i < N; i++){
        }
        _xend();
    } else { // Fallback path: get mutex
-       fb_mutex.acquire();
+       //fb_mutex.acquire();
+       fallBackLock.lock();
        sums[mygroup] += data[i];
-       fb_mutex.release();
+       //fb_mutex.release();
+       fallBackLock.unlock();
    }
 }
+cout<<"I am here"<<endl;
+cout<<"sums[20]="<<sums[20]<<endl;
 }
